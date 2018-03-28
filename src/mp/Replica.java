894f09bb799bc1f5b93d94d5aa9079a9c7f92c49
HaoranQi6 <haoranq2@illinois.edu>
2018-03-27 21:03:22 -0500

    modified Replica class2

--- src/mp/Replica.java
+++ src/mp/Replica.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 
 public abstract class Replica{
     public HashMap<Character, Integer> map = new HashMap<>();
-
+    public int W_value;
+    public int R_value;
     public abstract void write(Character key, Integer value);
     public abstract void read(Character key);
     public abstract void getWriteRequest(String message);
