80d62c7b3ef19358c91e698f3b40d2eb4b9ad4ca
HaoranQi6 <haoranq2@illinois.edu>
2018-03-28 15:15:36 -0500

    deleted hashmap from parent class

--- src/mp/Replica.java
+++ src/mp/Replica.java
@@ -3,7 +3,6 @@ package mp;
 import java.util.HashMap;
 
 public abstract class Replica {
-    public HashMap<Character, Integer> map = new HashMap<>();
 
     public abstract void write(Character key, Integer value);
     public abstract Integer read(Character key);
