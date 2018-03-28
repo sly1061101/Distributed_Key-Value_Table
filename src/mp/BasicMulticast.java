b4bb210fcd26643f0849c2e25e75fbd3762fd04c
sly1061101 <sly1061101@gmail.com>
2018-03-27 21:07:21 -0500

    Add comments for B-Multicast class

--- src/mp/BasicMulticast.java
+++ src/mp/BasicMulticast.java
@@ -1,23 +1,24 @@
 package mp;
 
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class BasicMulticast {
     Unicast u;
 
+    // BasicMulticast is implemented based on Unicast layer, so it need an Unicast object to initialize.
     public BasicMulticast(Unicast u) throws IOException {
         this.u = u;
     }
 
+    // multicast a message
     public void multicast(String message) throws IOException, InterruptedException {
         for (int i : u.hostInfo.idList)
             u.unicast_send(i, message);
     }
 
+    // deliever() method in BasicMulticast can deliever a message if there is a message in buffer.
+    // The return value is a string, in the format of "ID||message", where ID is the source of the message
+    // If there is currently no message, the method will return null.
     public String deliever() {
         for (int i : u.hostInfo.idList) {
             String message;
