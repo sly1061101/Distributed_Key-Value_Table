d8a07361e6460e9fcc805fb1dbd21ecc596ecc29
HaoranQi6 <haoranq2@illinois.edu>
2018-03-27 20:33:33 -0500

    modified Replica class

--- src/mp/Replica_env.java
+++ src/mp/Replica_env.java
@@ -4,19 +4,19 @@ public class Replica_env extends Replica{
     public  BasicMulticast m;
 
     @Override
-    public  void write(){
+    public  void write(Character key, Integer value){
         return;
-        }
+    }
     @Override
-    public  void read(){
+    public  void read(Character key){
         return;
-        }
+    }
     @Override
-    public  void getWriteRequest(){
+    public  void getWriteRequest(String message){
         return;
-        }
+    }
     @Override
-    public  void getReadRequest(){
+    public  void getReadRequest(String message){
         return;
-        }
+    }
 }
