b497c32d3751c51fae01361c3f375080ddb48a75
sly1061101 <sly1061101@gmail.com>
2018-03-30 19:51:07 -0500

    Modified main() method for better testing

--- src/main.java
+++ src/main.java
@@ -12,20 +12,16 @@ public class main {
     public static Client c;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-//        if(args.length != 2) {
-//            System.out.println("Wrong command line!\n");
-//            System.exit(-1);
-//        }
 
         Unicast u = new Unicast( Integer.parseInt(args[0]), Config.parseConfig("configFile") );
-        if(args[1].equals("Eventual") && args.length == 4){
+        if(args.length == 4 && args[1].equals("eve")){
             bm = new BasicMulticast(u);
             re = new Replica_env(bm);
             c = new Client(re, Integer.parseInt(args[0]));
             re.W_value = Integer.parseInt(args[2]);
             re.R_value = Integer.parseInt(args[3]);
         }
-        else if(args[1].equals("Linearizability")){
+        else if(args.length == 2 && args[1].equals("lin")){
             tm = new TotalMulticast(u);
 
             rl = new Replica_lin(tm);
@@ -34,7 +30,7 @@ public class main {
         }
 
         else{
-            System.out.println("Please choose one of the consistencies: Eventual or Linearizability. And formats: 'java main Eventual w_value r_value' or 'java main Linearizability'");
+            System.out.println("Please choose one of the consistencies: Eventual or Linearizability. And formats: 'java main ID eve w_value r_value' or 'java main ID lin'");
             return;
         }
 
@@ -43,18 +39,18 @@ public class main {
         while(true){
             String s = br.readLine();
             String[] strings = s.split(" ");
-            if(strings[0].equals("put")) {
+            if(strings.length == 3 && strings[0].equals("put")) {
                 char key = strings[1].charAt(0);
                 int value = Integer.valueOf(strings[2]);
                 c.put(key, value);
             }
-            else if(strings[0].equals("get")) {
+            else if(strings.length == 2 && strings[0].equals("get")) {
                 char key = strings[1].charAt(0);
                 c.get(key);
             }
             else if(strings[0].equals("dump"))
                 c.dump();
-            else if(strings[0].equals("delay")) {
+            else if(strings.length == 2 && strings[0].equals("delay")) {
                 int ms = Integer.valueOf(strings[1]);
                 Thread.sleep(ms);
                 //clear the input stream during sleeping
